8f3ec2ffe03ec6ef09a43a431df58aacafa9fb78
曹庆聪 <caoqingcong@corp.netease.com>
2020-09-29 19:19:35 +0800

    feat：更新部分操作

--- src/main/java/com/cqc/learning/java/file/FileTest.java
+++ src/main/java/com/cqc/learning/java/file/FileTest.java
@@ -1,15 +1,36 @@
 package com.cqc.learning.java.file;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * 文件测试类
  **/
 public class FileTest {
 
 	public static void main(String[] args) {
-		boolean flag = FileUtils.getInstance().isExists("D:\\", "test.txt");
-		System.out.println(flag);
 
-		System.out.println(FileUtils.getInstance().writeToFile("hello word", "test.txt", "D:\\"));
-
+		try {
+			//crm对应的邮箱
+			Set<String> crmEmailList = Files.lines(Paths.get("E:\\11.txt"), StandardCharsets.UTF_8)
+					.collect(Collectors.toSet());
+			//it对应的邮箱
+			Set<String> itEmailList = Files.lines(Paths.get("E:\\22.txt"), StandardCharsets.UTF_8)
+					.collect(Collectors.toSet());
+			//取差集
+			Set<String> subList = itEmailList.stream().filter(item -> (!crmEmailList.contains(item)))
+					.collect(Collectors.toSet());
+			//取差集
+			Set<String> subList2 = crmEmailList.stream().filter(item -> (!itEmailList.contains(item)))
+					.collect(Collectors.toSet());
+			System.out.println(subList);
+			System.out.println(subList2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
